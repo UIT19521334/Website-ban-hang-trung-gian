510ea85443691e9e40a1b47337b18ec5fc6b9c1d
UIT.19521334 <67258104+UIT19521334@users.noreply.github.com>
2022-03-14 20:18:28 +0700

    deadelte

--- src/components/badge/Badge.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./badge.css";
-
-const Badge = (props) => {
-  return <span className={`badge badge-${props.type}`}>{props.children}</span>;
-};
-
-export default Badge;
