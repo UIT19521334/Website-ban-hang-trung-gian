abfe410b1da7d97c7e9f78cea395df3ef394888a
UIT.19521334 <67258104+UIT19521334@users.noreply.github.com>
2022-03-24 14:32:57 +0700

    code layout

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
 const handlebars= require('express-handlebars');
 const app = express();
 const port = 3000;
+
+//Use foler img
+app.use(express.static(path.join(__dirname,'public')));
+
 //HTTP logger
 app.use(morgan('combined'));
-// Template engine
-app.engine('handlebars', handlebars.engine());
-app.set('view engine', 'handlebars');
 
+// Template engine
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname,'resources/views'));
+
+// Route
 app.get('/', (req, res) => {
   res.render('home');
 });
+app.get('/news', (req, res) => {
+  res.render('news');
+});
 
+//Port ở đây là local host
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
