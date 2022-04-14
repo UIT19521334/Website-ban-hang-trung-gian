23e3981d8ffb9dec0b1a25736f5bdfa88ff383c5
nguynphucc <19520224@gm.uit.edu.vn>
2022-04-14 13:33:48 +0700

    update back end signin signup

---
+++ app-back-end/src/index.server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import env from 'dotenv';
+import bodyParser from 'body-parser';
+
+//routes
+import userRoutes from './routes/auth.js';
+
+const app = express();
+
+//enviroment variable or you can say constants
+env.config();
+
+//mongodb connection
+//mongodb+srv://sfz:<password>@cluster0.c3klu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.c3klu.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log('Database connected');
+    });
+
+app.use(bodyParser());
+app.use('/api', userRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
