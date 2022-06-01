f256aadf798e8fdcbf719b32f0ea79baa62171b7
Nguyễn Ngọc Phúc <19520224@gm.uit.edu.vn>
2022-06-01 22:10:14 +0700

    Fix bug

--- app-back-end/src/index.server.js
+++ app-back-end/src/index.server.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import env from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import env from "dotenv";
 
 //routes
-import userRoutes from './routes/auth.js';
-import adminRoutes from './routes/admin/auth.js';
-import categoryRoutes from './routes/category.js'
+import userRoutes from "./routes/auth.js";
+import adminRoutes from "./routes/admin/auth.js";
+import categoryRoutes from "./routes/category.js";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -15,17 +16,20 @@ env.config();
 //mongodb connection
 //mongodb+srv://sfz:<password>@cluster0.c3klu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.c3klu.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`)
-    .then(() => {
-        console.log('Database connected');
-    });
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.c3klu.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Database connected");
+  });
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.use('/api', userRoutes);
-app.use('/api', adminRoutes);
-app.use('/api', categoryRoutes);
+app.use("/api", userRoutes);
+app.use("/api", adminRoutes);
+app.use("/api", categoryRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
--- app-back-end/src/routes/auth.js
+++ app-back-end/src/routes/auth.js
@@ -1,20 +1,28 @@
-import express from 'express';
-import { validateSignupRequest, isRequestValidated, validateSigninRequest } from '../validators/auth.js';
-import { requireSignin, signin, signup } from '../controller/auth.js'
-import { getPosts , createPosts , updatePosts } from '../controller/productController.js';
+import express from "express";
+import {
+  validateSignupRequest,
+  isRequestValidated,
+  validateSigninRequest,
+} from "../validators/auth.js";
+import { signin, signup } from "../controller/auth.js";
+import {
+  getPosts,
+  createPosts,
+  updatePosts,
+} from "../controller/productController.js";
 const router = express.Router();
 
-router.post('/signin', validateSigninRequest, isRequestValidated, signin);
+router.post("/signin", validateSigninRequest, isRequestValidated, signin);
 
-router.post('/signup', validateSignupRequest, isRequestValidated, signup);
+router.post("/signup", validateSignupRequest, isRequestValidated, signup);
 
 // router.post('/profile', requireSignin, (req, res) => {
 //     res.status(200).json({ user: 'profile' })
 // });
 
-router.get('/', getPosts);
-router.post('/',createPosts);
-router.post('/update',updatePosts);
+router.get("/", getPosts);
+router.post("/", createPosts);
+router.post("/update", updatePosts);
 //module.exports = router;
 
 export default router;
