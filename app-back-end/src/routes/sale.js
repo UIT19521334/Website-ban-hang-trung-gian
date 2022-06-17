d61125a34b659ed9ec8786939c2df4d18a4a00fc
Nguyễn Ngọc Phúc <19520224@gm.uit.edu.vn>
2022-06-17 17:32:47 +0700

    fix bug

--- app-back-end/src/routes/sale.js
+++ app-back-end/src/routes/sale.js
@@ -1,18 +1,11 @@
 import express from "express";
-import {
-  create,
-  getAll,
-  update,
-  deleteById,
-  getByProductId,
-} from "../controller/sale.js";
+import { create, getAll, update, deleteById } from "../controller/sale.js";
 
 const router = express.Router();
 
 router.post("/sale/create", create);
 
 router.get("/sale/getAll", getAll);
-router.get("/sale/:id", getByProductId);
 
 router.put("/sale/:id", update);
 
