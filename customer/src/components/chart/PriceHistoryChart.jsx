dd4f9f98a5ef2ba17b43fd339e02dc98f71fe344
Nguyễn Ngọc Phúc <19520224@gm.uit.edu.vn>
2022-06-19 18:05:56 +0700

    done

--- customer/src/components/chart/PriceHistoryChart.jsx
+++ customer/src/components/chart/PriceHistoryChart.jsx
@@ -1,4 +1,4 @@
-import { Chart as ChartJS } from 'chart.js/auto'; 
+import { Chart as ChartJS } from "chart.js/auto";
 import React from "react";
 import { Line } from "react-chartjs-2";
 import { MDBContainer } from "mdbreact";
@@ -9,28 +9,7 @@ class PriceHistoryChart extends React.Component {
       labels: ["January", "February", "March", "April", "May", "June", "July"],
       datasets: [
         {
-          label: "Ask",
-          fill: true,
-          lineTension: 0.3,
-          backgroundColor: "rgba(225, 204,230, .3)",
-          borderColor: "rgb(205, 130, 158)",
-          borderCapStyle: "butt",
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
-          pointBorderColor: "rgb(205, 130,1 58)",
-          pointBackgroundColor: "rgb(255, 255, 255)",
-          pointBorderWidth: 10,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgb(0, 0, 0)",
-          pointHoverBorderColor: "rgba(220, 220, 220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-          label: "Bid",
+          label: "Sale",
           fill: true,
           lineTension: 0.3,
           backgroundColor: "rgba(184, 185, 210, .3)",
@@ -48,16 +27,20 @@ class PriceHistoryChart extends React.Component {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [28, 48, 40, 19, 86, 27, 90]
-        }
-      ]
-    }
+          data: [28, 48, 40, 19, 86, 27, 90],
+        },
+      ],
+    },
   };
 
   render() {
     return (
       <MDBContainer>
-        <Line data={this.state.dataLine} options={{ responsive: true }} style={{maxHeight: '200px',}} />
+        <Line
+          data={this.state.dataLine}
+          options={{ responsive: true }}
+          style={{ maxHeight: "200px" }}
+        />
       </MDBContainer>
     );
   }
